16b5c2cff17152a0e24017e5fe1b36634a1bbe6d
Yashovardhan1311 <yashvardhan.pratap@gmail.com>
2025-06-09 00:49:38 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FormComponent from './Components/form-component';
+import './styles.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <FormComponent />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
